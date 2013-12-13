035843de850c8e7c7d93301565aae40ccbe51687
pujarashmi <pujarashmi10@gmail.com>
2013-12-13 14:53:04 +0530

    Using Flash

---
+++ flash/app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var flash=require('connect-flash');
+
+var app = express();
+
+app.configure(function() {
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session({ cookie: { maxAge: 60000 }}));
+  app.use(flash());
+});
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/flash', function(req, res){
+  req.flash('info', 'Hi there!')
+  res.redirect('/');
+});
+
+app.get('/no-flash', function(req, res){
+    req.flash('info', ['Welcome', 'Please Enjoy']);
+    res.redirect('/');
+});
+
+app.get('/', function(req, res){
+  res.render('index', { message: req.flash('info') });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
